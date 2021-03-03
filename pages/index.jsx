0d3ba6f110ad3c39596df6274c83f3bdd651d75a
benyoss <benyoss4@gmail.com>
2021-03-02 22:43:22 -0600

    (insert) splashpage header w/ animation tween

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,30 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import Link from 'next/link';
-import About from '../components/App';
+import { useState, useEffect } from 'react';
 
-const Test2 = () => (
-    <div>
-        <h3>Hello Next with typescript!</h3>
-        <Link href="/about">
-            <a title="Click me!!">
-                Click me!!
-            </a>
-        </Link>
-        <About />
+const Index = () => {
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [windowHeight, setWindowHeight] = useState();
+
+  useEffect(() => {
+    setWindowHeight(window.innerHeight);
+    setWindowWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div className="splash-container" width={windowWidth} height={windowHeight}>
+        <div className="container-header">
+            <h3 className="header-title">{'W E L C O M E'}</h3>
+        </div>
+        <div className="header-divider">
+        </div>
+        <div className="container-body">
+            <div className="three-env">
+                <h2>This will be the 3D enviornment</h2>
+            </div>
+        </div>
     </div>
-);
+  );
+};
 
-export default Test2;
+export default Index;
