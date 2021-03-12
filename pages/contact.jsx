44423c6323ce5de31b2cf885dc9891cb89a0de44
benyoss <benyoss4@gmail.com>
2021-03-12 01:38:56 -0600

    (fix) form issue

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -30,6 +30,7 @@ export default function Contact({ transClick, setTransClick }) {
         </div>
         <div class="screen-body-item">
           <form name="app-form" data-netlify="true" method="POST" class="app-form">
+            <input type="hidden" name="form-name" value="app-form" />
             <div class="app-form-group">
               <input class="app-form-control" placeholder="NAME" />
             </div>
@@ -40,8 +41,8 @@ export default function Contact({ transClick, setTransClick }) {
               <textarea class="app-form-control" placeholder="MESSAGE" />
             </div>
             <div class="app-form-group buttons">
-              <button class="app-form-button">CANCEL</button>
-              <button class="app-form-button">SEND</button>
+              <button type="reset" class="app-form-button">CANCEL</button>
+              <button type="submit" class="app-form-button">SEND</button>
             </div>
           </form>
         </div>
